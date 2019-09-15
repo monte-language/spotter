2402da8a7b5d9215b6075fbfa500ca99eb0bc4e7
Corbin <cds@corbinsimpson.com>
2019-09-15 11:28:53 -0700

    Get all of the right structure in place.
    It's good to know already that there are subsystems like Scanf that make
    a lot of sense for use cases that are parallel to, but not quite shaped
    like, Monte use cases. Necessary experiments.

--- spotter.ml
+++ spotter.ml
@@ -69,41 +69,83 @@ mastpatt = IgnorePattern of mastexpr
          | BindingPatt of string
 ;;
 
-exception InvalidMAST of string;;
+exception InvalidMAST of (string * char * int);;
+let throw_invalid_mast ic c message = raise (InvalidMAST (message, c, pos_in ic))
 
-let scan_varint ib =
-    let rec go shift acc = Scanf.bscanf ib "%c" (fun c ->
-        let b = Z.of_int (int_of_char c) in
+let input_varint ic =
+    let rec go shift acc =
+        let b = Z.of_int (input_byte ic) in
         let n = Z.logor acc (Z.shift_left (Z.logand b (Z.of_int 0x7f)) shift) in
         if not (Z.testbit b 7) then n else go (shift + 7) n
-    ) in go 0 Z.zero;;
+    in go 0 Z.zero;;
 
 type span = OneToOne of (Z.t * Z.t * Z.t * Z.t)
           | Blob of (Z.t * Z.t * Z.t * Z.t)
 ;;
-let scan_span ib = Scanf.bscanf ib "%1[SB]%r%r%r%r" scan_varint scan_varint
-    scan_varint scan_varint (fun c i1 i2 i3 i4 -> match c with
-        | "S" -> OneToOne (i1, i2, i3, i4)
-        | "B" -> Blob (i1, i2, i3, i4)
-        (* This match is impossible because of the earlier range restriction. *)
-        |  x -> raise (InvalidMAST x)
-);;
-
-let scan_expr ib = Scanf.bscanf ib "%c" (fun c -> match c with
-    | 'L' -> Scanf.bscanf ib "%c" (fun c -> match c with
-        | 'N' -> NullExpr
-        |  x  -> raise (InvalidMAST (String.make 1 x)))
-    |  x  -> raise (InvalidMAST (String.make 1 x)))
+let input_span ic = match input_char ic with
+    | 'S' -> OneToOne (input_varint ic, input_varint ic, input_varint ic, input_varint ic)
+    | 'B' -> Blob (input_varint ic, input_varint ic, input_varint ic, input_varint ic)
+    |  x  -> throw_invalid_mast ic x "input_span"
 ;;
 
-let monte_of_mast ib = Scanf.bscanf ib "%r%r" scan_expr scan_span
-    (fun expr sp -> (expr, sp));;
+let input_str ic = really_input_string ic (Z.to_int (input_varint ic));;
+
+(* A growing mutable list that is indexed backwards. Simulates a portion of
+ * the Python list API. *)
+let backlist () = object
+    val mutable l = []
+    val mutable len = 0
+    method push x = l <- x :: l; len <- len + 1
+    method get i = List.nth l (len - 1 - i)
+    method tl = List.hd l
+end;;
+
+let mast_context ic = object (self)
+    (* Compared to the classic MAST context, we store the exprs and patts
+     * backwards, so that we can build them quickly. *)
+    val exprs = backlist ()
+    val patts = backlist ()
+
+    method private input_expr_ref = fst (exprs#get (Z.to_int (input_varint ic)))
+    method private input_expr c = match c with
+        | 'L' -> (match input_char ic with
+            | 'N' -> NullExpr
+            |  x  -> throw_invalid_mast ic x "literal"
+        )
+        | 'N' -> NounExpr (input_str ic)
+        |  x  -> throw_invalid_mast ic x "input_expr"
+    method private input_patt = match input_char ic with
+        | 'I' -> IgnorePattern self#input_expr_ref
+        | 'F' -> let n = input_str ic in FinalPattern (n, self#input_expr_ref)
+        | 'V' -> let n = input_str ic in VarPattern (n, self#input_expr_ref)
+        |  x  -> throw_invalid_mast ic x "input_patt"
+    method private input_expr_and_span c =
+        let e = self#input_expr c in (e, input_span ic)
+    method private input_patt_and_span =
+        let p = self#input_patt in (p, input_span ic)
+
+    method input_all_exprs =
+        try while true do
+            match input_char ic with
+                | 'P' -> patts#push self#input_patt_and_span
+                |  c  -> exprs#push (self#input_expr_and_span c)
+        done with | End_of_file -> ()
+    method input_last_expr = self#input_all_exprs; exprs#tl
+end;;
+
+exception InvalidMagic;;
+let mast_magic = "Mont\xe0MAST\x01";;
+let open_in_mast path = let ic = open_in_bin path in
+    (* Check the magic number. *)
+    for i = 0 to String.length mast_magic - 1 do
+        if input_char ic <> String.get mast_magic i then
+            (close_in ic; raise InvalidMagic)
+    done; ic
+;;
 
 let read_mast filename =
-    let ic = open_in_bin filename in
-    let ib = Scanf.Scanning.from_channel ic in
-    (* Check the magic number. *)
-    Scanf.bscanf ib "Mont\xe0MAST\x01" ();
-    let rv = monte_of_mast ib in
+    let ic = open_in_mast filename in
+    let context = mast_context ic in
+    let rv = context#input_last_expr in
     close_in ic;
     rv;;
